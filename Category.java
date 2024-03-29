222d9c643d659d9ade630d3cd44e7c27c243c86f
Hessam <hessam2004h@gmail.com>
2024-03-29 21:53:09 +0330

    Category Final

--- Category.java
+++ Category.java
@@ -1,17 +1,28 @@
 import java.util.ArrayList;
 
+/**
+ * The Category class represents a category in the library system.
+ * 
+ * @author Hessam Hosseinian
+ */
 public class Category {
 
     private String categoryId;
     private String categoryName;
-
     private ArrayList<Category> subCategory;
 
+    /**
+     * Constructs a new Category object with the specified attributes.
+     *
+     * @param categoryId   The unique identifier of the category.
+     * @param categoryName The name of the category.
+     */
     public Category(String categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
         subCategory = new ArrayList<>();
     }
+    // !-------------------------------------------------CATEGORY_ID
 
     public String getCategoryId() {
         return this.categoryId;
@@ -20,6 +31,7 @@ public class Category {
     public void setCategoryId(String categoryId) {
         this.categoryId = categoryId;
     }
+    // !-------------------------------------------------CATEGORY_NAME
 
     public String getCategoryName() {
         return this.categoryName;
@@ -28,6 +40,7 @@ public class Category {
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
     }
+    // !-------------------------------------------------SUB_CATEGORY
 
     public ArrayList<Category> getSubCategory() {
         return this.subCategory;
