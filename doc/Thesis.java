6f31c7952a2942aa1e1536f26957721e05fc273a
Hessam <hessam2004h@gmail.com>
2024-03-29 21:26:39 +0330

    Thesis Final

--- doc/Thesis.java
+++ doc/Thesis.java
@@ -1,7 +1,25 @@
 package doc;
 
+/**
+ * The Thesis class represents a thesis document in a library system.
+ * It extends the Document class and inherits its properties and methods.
+ */
 public class Thesis extends Document {
 
+    private String studentName;
+    private String professorName;
+
+    /**
+     * Constructs a new Thesis object with the specified attributes.
+     *
+     * @param docId           The unique identifier of the thesis document.
+     * @param title           The title of the thesis.
+     * @param studentName     The name of the student who authored the thesis.
+     * @param professorName   The name of the professor who supervised the thesis.
+     * @param publicationYear The publication year of the thesis.
+     * @param categoryId      The category ID of the thesis.
+     * @param libraryId       The library ID where the thesis is located.
+     */
     public Thesis(String docId, String title, String studentName, String professorName, String publicationYear,
             String categoryId, String libraryId) {
         super(docId, title, "-", "-", publicationYear, 1, categoryId, libraryId);
@@ -10,20 +28,21 @@ public class Thesis extends Document {
 
     }
 
-    private String studentName;
-    private String professorName;
-
+    // !--------------------------------------------STUDENT_NAME
     public String getStudentName() {
         return this.studentName;
     }
+    // ?---------------------------------------------
 
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
+    // !--------------------------------------------PROFESSOR_NAME
 
     public String getProfessorName() {
         return this.professorName;
     }
+    // ?---------------------------------------------
 
     public void setProfessorName(String professorName) {
         this.professorName = professorName;
