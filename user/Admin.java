83f627ad7e8056568bdf5a258f47f6012f67b3f3
Hessam <hessam2004h@gmail.com>
2024-03-29 21:42:45 +0330

    Admin Final

--- user/Admin.java
+++ user/Admin.java
@@ -1,12 +1,27 @@
 package user;
 
+/**
+ * The Admin class represents an administrator user in the system.
+ * It extends the User class and inherits its properties and methods.
+ * 
+ * @author Hessam Hosseinian
+ */
 public class Admin extends User {
-
+    /**
+     * Constructs a new Admin object with the specified attributes.
+     *
+     * @param userId     The unique identifier of the admin user.
+     * @param password   The password of the admin user.
+     * @param firstName  The first name of the admin user.
+     * @param lastName   The last name of the admin user.
+     * @param nationalId The national ID of the admin user.
+     * @param birthDay   The birth date of the admin user.
+     * @param address    The address of the admin user.
+     */
     public Admin(String userId, String password, String firstName, String lastName, String nationalId, String birthDay,
             String address) {
         super(userId, password, firstName, lastName, nationalId, birthDay, address);
-        //TODO Auto-generated constructor stub
+
     }
-    
 
 }
