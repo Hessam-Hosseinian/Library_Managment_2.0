a69cce4c46d3e7eade6578bd224b35cc8d9843a0
Hessam <hessam2004h@gmail.com>
2024-03-29 21:45:49 +0330

    Professor Final

--- user/Professor.java
+++ user/Professor.java
@@ -1,11 +1,28 @@
 package user;
 
+/**
+ * The Professor class represents a professor user in the system.
+ * It extends the User class and inherits its properties and methods.
+ * 
+ * @author Hessam Hosseinian
+ */
 public class Professor extends User {
 
+    /**
+     * Constructs a new Professor object with the specified attributes.
+     *
+     * @param userId     The unique identifier of the professor user.
+     * @param password   The password of the professor user.
+     * @param firstName  The first name of the professor user.
+     * @param lastName   The last name of the professor user.
+     * @param nationalId The national ID of the professor user.
+     * @param birthDay   The birth date of the professor user.
+     * @param address    The address of the professor user.
+     */
     public Professor(String userId, String password, String firstName, String lastName, String nationalId,
             String birthDay, String address) {
         super(userId, password, firstName, lastName, nationalId, birthDay, address);
-        // TODO Auto-generated constructor stub
+
     }
 
 }
