2841fada1b03e363143d716afc8d87a1c52853f7
Hessam <hessam2004h@gmail.com>
2024-03-29 21:44:38 +0330

    Manager Final

--- user/Manager.java
+++ user/Manager.java
@@ -1,15 +1,34 @@
 package user;
 
+/**
+ * The Manager class represents a manager user in the system.
+ * It extends the User class and inherits its properties and methods.
+ * 
+ * @author Hessam Hosseinian
+ */
 public class Manager extends User {
 
+    private String libraryId;
+
+    /**
+     * Constructs a new Manager object with the specified attributes.
+     *
+     * @param userId     The unique identifier of the manager user.
+     * @param password   The password of the manager user.
+     * @param firstName  The first name of the manager user.
+     * @param lastName   The last name of the manager user.
+     * @param nationalId The national ID of the manager user.
+     * @param birthDay   The birth date of the manager user.
+     * @param address    The address of the manager user.
+     * @param libraryId  The ID of the library managed by the manager.
+     */
     public Manager(String userId, String password, String firstName, String lastName, String nationalId,
             String birthDay, String address, String libraryId) {
         super(userId, password, firstName, lastName, nationalId, birthDay, address);
         this.libraryId = libraryId;
     }
 
-    private String libraryId;
-
+    // !---------------------------------------------LIBRARY_ID
     public String getLibraryId() {
         return this.libraryId;
     }
