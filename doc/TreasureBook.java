0124c5adfc3f7a2078b5442670c0be7105c7aab6
Hessam <hessam2004h@gmail.com>
2024-03-29 21:28:15 +0330

    TreasureBook Final

--- doc/TreasureBook.java
+++ doc/TreasureBook.java
@@ -1,7 +1,26 @@
 package doc;
 
+/**
+ * The TreasureBook class represents a special book document in a library system
+ * that is considered a treasure. It extends the Document class and inherits its
+ * properties and methods.
+ */
 public class TreasureBook extends Document {
 
+    private String donor;
+
+    /**
+     * Constructs a new TreasureBook object with the specified attributes.
+     *
+     * @param docId           The unique identifier of the treasure book document.
+     * @param title           The title of the treasure book.
+     * @param author          The author of the treasure book.
+     * @param publisher       The publisher of the treasure book.
+     * @param publicationYear The publication year of the treasure book.
+     * @param donor           The name of the donor who provided the treasure book.
+     * @param categoryId      The category ID of the treasure book.
+     * @param libraryId       The library ID where the treasure book is located.
+     */
     public TreasureBook(String docId, String title, String author, String publisher, String publicationYear,
             String donor, String categoryId, String libraryId) {
         super(docId, title, author, publisher, publicationYear, 1, categoryId, libraryId);
@@ -9,8 +28,7 @@ public class TreasureBook extends Document {
         this.donor = donor;
     }
 
-    private String donor;
-
+    // !--------------------------------------------DONOR
     public String getDonor() {
         return this.donor;
     }
