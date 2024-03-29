eb26eb10c863b25914d711fb1fd59991cfc16197
Hessam <hessam2004h@gmail.com>
2024-03-29 21:58:56 +0330

    Read Final

--- Read.java
+++ Read.java
@@ -1,23 +1,35 @@
 import java.sql.Date;
 
+/**
+ * The Read class represents a record of a user reading a book in the library
+ * system.
+ * 
+ * @author Hessam Hosseinian
+ */
 public class Read {
 
     private String userId;
     private String libraryId;
     private String bookId;
+    private Date date;
 
-    private Date date1;
-    // private String startDate;
-    // private String endDate;
-
-    public Read(String userId, String libraryId, String bookId, Date date1) {
+    /**
+     * Constructs a new Read object with the specified attributes.
+     *
+     * @param userId    The unique identifier of the user who read the book.
+     * @param libraryId The ID of the library where the reading occurred.
+     * @param bookId    The unique identifier of the book being read.
+     * @param date      The date when the reading occurred.
+     */
+    public Read(String userId, String libraryId, String bookId, Date date) {
         this.userId = userId;
         this.libraryId = libraryId;
         this.bookId = bookId;
-        this.date1 = date1;
+        this.date = date;
 
     }
 
+    // !------------------------------------------USER_ID
     public String getUserId() {
         return this.userId;
     }
@@ -25,6 +37,7 @@ public class Read {
     public void setUserId(String userId) {
         this.userId = userId;
     }
+    // !------------------------------------------LIBRARY_ID
 
     public String getLibraryId() {
         return this.libraryId;
@@ -33,6 +46,7 @@ public class Read {
     public void setLibraryId(String libraryId) {
         this.libraryId = libraryId;
     }
+    // !------------------------------------------BOOK_ID
 
     public String getBookId() {
         return this.bookId;
@@ -41,13 +55,14 @@ public class Read {
     public void setBookId(String bookId) {
         this.bookId = bookId;
     }
+    // !------------------------------------------DATE
 
     public Date getDate1() {
-        return this.date1;
+        return this.date;
     }
 
     public void setDate1(Date date1) {
-        this.date1 = date1;
+        this.date = date1;
     }
 
 }
